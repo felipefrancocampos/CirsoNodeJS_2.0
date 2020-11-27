8fcb9d798a9b42f8b6d6032b3ee58ed9576afdb6
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2020-11-27 16:51:22 -0300

    Corrigi erros ortográficos.

--- Cap02/noticias.js
+++ Cap02/noticias.js
@@ -3,19 +3,17 @@ var http =  require('http')
 var server = http.createServer(function(req, res){
     var categoria = req.url
    
-    if(categoria == 'tecnologia'){
-        res.end("<html><body>Notícias de Tecnologia</body></html>")
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Noticias de Tecnologia</body></html>")
 
-    } else if (categoria == 'moda') {
-        res.end("<html><body>Notícias de Moda</body></html>")
+    } else if (categoria == '/moda') {
+        res.end("<html><body>Noticias de Moda</body></html>")
    
-    } else if (categoria == 'beleza') {
-        res.end("<html><body>Notícias de Beleza</body></html>")
+    } else if (categoria == '/beleza') {
+        res.end("<html><body>Noticias de Beleza</body></html>")
     
     } else {
-        res.end("<html><body>Portal de notícias</body></html>")
+        res.end("<html><body>Portal de Noticias</body></html>")
     }
     
-}) 
-
-server.listen(3000)
+}).listen(3000)
