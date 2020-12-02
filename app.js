a72867ca49483a03f8912b8d1540403823040ca9
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2020-12-02 18:11:07 -0300

    Continuação Curso

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res){
-    res.render("<html><body>Portal de Noticias</body></html>")
+app.get('/tecnologia', function(req, res){
+    res.render("secao/tecnologia")
 })
 
-app.get('/tecnologia', function(req, res){
-    res.render("secao/tecnologia.ejs")
+app.get('/', function(req, res){
+    res.send("<html><body>Portal de Noticias</body></html>")
 })
 
 app.listen(3000, function(){
