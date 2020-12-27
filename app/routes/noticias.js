0155b651f8184407e40cc70f19624342582cd71d
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2020-12-27 14:01:04 -0300

    Continuation

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,12 @@
-var dbConnection = require('../../config/dbConnection')
-
 module.exports = function(app) {
-
-    var connection = dbConnection()
 
     app.get('/noticias', function(req, res){
 
-        connection.query('select * from noticias', function(error, result){
+        var connection = app.config.dbConnection()
+        var noticiasModel = app.app.models.noticiasModel
+
+        noticiasModel.getNoticias(connection, function(error, result){
             res.render("noticias/noticias", {noticias : result})
         })
-
     })
 }
-    
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,15 @@
 var mysql = require('mysql')
 
-module.exports = function() {
+var connMySQL = function(){
+    console.log('Conexao com o banco de dados foi estabelecida')
     return mysql.createConnection({
         host : 'localhost', 
         user : 'root',
         pasword : 'Felps542543',
         database : 'portal_noticias'
     })
-} 
+}
+module.exports = function() {
+    console.log('O autoload carregou o módulo de conexão com bd')
+    return connMySQL
+}
