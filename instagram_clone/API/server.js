cb619632738a02827ef6118d3eeca796438a4e41
Felipe Franco Campos <felipe.francocampos024@gmail.com>
2021-02-26 02:26:39 -0300

    .

--- instagram_clone/API/server.js
+++ instagram_clone/API/server.js
@@ -1,12 +1,16 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb');
+    multiparty = require('connect-multiparty'),
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectId
+    fs = require('fs');
 
 var app = express();
 
-
+//body-parser
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var porta = 8080;
 
@@ -25,21 +29,109 @@ app.get('/', function(req, res){
     res.send({msg:'Olá'});
 });
 
+//POST (create)
 app.post('/api', function(req, res){
+
+    res.setHeader("Acess-Control_Allow_Origin", "*");
 
     var dados = req.body;
 
-    db.prependOnceListener(function(err, mongoclient){
+    res.send(dados);
+
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + req.files.arquivo.originalFilename;
+
+    fs.rename(path_origem, path_destino)
+
+    /*
+    db.open( function(err, mongoclient){
         mongoclient.collection('postagens', function(err, collection){
            collection.insert(dados, function(err, records){
                if(err){
-                    res.json(err);
+                    res.json({'status' : 'erro'});
                }else {
-                   res.json(records);
+                   res.json({'status' : 'inclusao realizada com sucesso'});
                }
                mongoclient.close();
             });
         });
+    });
+    */
 
+});
+
+
+//GET (ready)
+app.get('/api', function(req, res){
+    db.open( function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find().toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                } else {
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+
+});
+
+
+//GET by ID (ready)
+app.get('/api/:id', function(req, res){
+    db.open( function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find(objectId(req.params.id)).toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                } else {
+                    res.status(200).json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+
+});
+
+
+//PUT by ID (update)
+app.put('/api/:id', function(req, res){
+    db.open( function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.update(
+                { _id : objectId(req.params.id) },
+                { $set : { titulo : req.body.titulo }},
+                {},
+                function(err, records){
+                    if(err){
+                        res.json(err);
+                    } else {
+                        res.json(records);
+                    }
+
+                    mongoclient.close();
+                }
+            );
+        });
+    });
+});
+
+
+//DELETE by ID (remover)
+app.delete('/api/:id', function(req, res){
+    db.open( function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.remove({ _id : objectId(req.params.id)}, function(err, records){
+                if(err){
+                    res.json(err);
+                } else {
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        });
     });
 });
